abc49fb3326f6193d6700b659a629a51f8a19bb3
Jeff Hollister <jeff.w.hollister@gmail.com>
2015-06-17 10:31:36 -0400

    automatic build of site

---
+++ _site/rmd_posts/2015-06-16-00-Before-The-Workshop.R
@@ -0,0 +1,12 @@
+## ----version, eval=FALSE-------------------------------------------------
+## version$version.string
+
+## ----version2, echo=FALSE------------------------------------------------
+version$version.string
+
+## ----git_version, eval=FALSE---------------------------------------------
+## system("git --version")
+
+## ----git_version2, echo=FALSE--------------------------------------------
+system("git --version")
+
